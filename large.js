0b50b243513facc82a85fdb003f021e5ae4eb64c
NooR Nabiji <noornabiji4@gmail.com>
2018-08-02 12:03:45 +0530

    large

---
+++ large.js
@@ -0,0 +1,10 @@
+var amount = prompt('enter the amount');
+		if (amount > 10){
+			amount*3;
+			console.log(amount)
+			alert (amount+' amount is large')
+		}
+		else {
+			alert (+ 'amount is low')
+		}
+		console.log('Hello,World');
