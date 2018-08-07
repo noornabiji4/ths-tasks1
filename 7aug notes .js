3782fb355f0172d7741d1ff0d91f99fc8bd64bdf
NooR Nabiji <noornabiji4@gmail.com>
2018-08-07 22:37:30 +0530

    today Notes

---
+++ 7aug notes .js
@@ -0,0 +1,34 @@
+                .this 
+		prototype
+		.call()
+		this keyord call a window is a globel object
+		recall function using this keyword
+		count value true
+
+		what is call stack call side
+		default binding
+		defrence between let and var 
+		remeber of scope value
+		lexical refrence to scope
+		this kew ord refer to lexixcal scope
+		call side
+		call stack
+		default call
+		context
+		precided
+		implicity 
+		explicity
+		hard binding
+		apply call function
+		.call function
+		diffrence between applying and bindind call
+		.bind
+		.call
+		.apply
+		function prototype
+		difrence between call apply bing with example
+		new binding
+		function prototype study by tommarow
+		new
+		newly constractor with object then call an apply
+		difrense betw
