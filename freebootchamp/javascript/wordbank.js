d9233421c1cc5e7234226eeac7daed70a0ec2bb8
NooR Nabiji <noornabiji4@gmail.com>
2018-08-10 20:09:52 +0530

    Word Bank

---
+++ freebootchamp/javascript/wordbank.js
@@ -0,0 +1,12 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+result += "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
+
